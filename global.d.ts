dea917d9fa58ae068dc76b686234b9f4d7e99348
HARCHHI <harc.maxwin@gmail.com>
2024-01-30 00:12:04 +0800

    ci(configs): 調整lnit設定及.gitignore

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="jest-extended" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    [key: string]: string | undefined;
+    DB_TYPE: 'postgres';
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_DATABASE: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+  }
+}
