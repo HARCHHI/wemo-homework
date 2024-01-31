a781347676ffb66878924a1c349071eb7e87a070
HARCHHI <harc.maxwin@gmail.com>
2024-02-01 00:43:40 +0800

    feat(validation): 加入validation模組以驗證input body

--- src/scooters/dto/scooter.dto.ts
+++ src/scooters/dto/scooter.dto.ts
@@ -1,11 +1,19 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class CreateScooterDto {
+  @IsString()
   plate: string;
 
+  @IsNumber()
   typeId: number;
 }
 
 export class UpdateScooterDto {
+  @IsString()
+  @IsOptional()
   plate?: string;
 
+  @IsNumber()
+  @IsOptional()
   typeId?: number;
 }
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,13 +1,23 @@
+import { IsString, IsNumber, IsOptional, IsBoolean } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   name: string;
 
+  @IsNumber()
   age: number;
 }
 
 export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
   name?: string;
 
+  @IsNumber()
+  @IsOptional()
   age?: number;
 
+  @IsBoolean()
+  @IsOptional()
   isActive?: boolean;
 }
